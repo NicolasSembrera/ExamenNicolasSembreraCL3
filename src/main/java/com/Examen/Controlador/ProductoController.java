c0869bb9d4f586e345cb842100fbb0fb914ada81
NicolasSembrera <nsembrera@gmail.com>
2023-11-26 01:43:46 -0500

    Paso 2 prueba

--- src/main/java/com/Examen/Controlador/ProductoController.java
+++ src/main/java/com/Examen/Controlador/ProductoController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.Examen.Modelo.Producto;
@@ -21,7 +24,6 @@ public class ProductoController {
 	@GetMapping("/ListaProducto")
 	public String ListadoProducto(Model model) {
 		List<Producto> lista = iproductoservicio.ListadoProducto();
-
 		model.addAttribute("listaproductos", lista);
 		return "Vistas/Producto/ListaProducto";
 	}
@@ -29,8 +31,15 @@ public class ProductoController {
 	@GetMapping("/RegistrarProducto")
 	public String RegistrarProducto(Model model) {
 		Producto producto = new Producto();
-		model.addAttribute("RegistraProducto", producto);
-		return "Vistas/Auto/FrmRegistraProducto";
+		model.addAttribute("productito", producto);
+		return "Vistas/Producto/FrmRegistraProducto";
 	}
+	
+	@PostMapping("/InsertarProducto")
+	public String GuardarAuto(@ModelAttribute Producto producto) {
+		iproductoservicio.RegistrarAuto(producto);	
+		return "redirect:/ControlProducto/ListaProducto";
+	} 
+	
 
 }
